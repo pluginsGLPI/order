2abd6cd1dfd4a1d84063f2df73b702ffcd26f395
tsmr <tsmr@thetsmr.fr>
2009-12-30 22:48:19 +0000

    retour arriere
    git-svn-id: https://forge.glpi-project.org/svn/order/trunk@212 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_order.dropdown.function.php
+++ inc/plugin_order.dropdown.function.php
@@ -212,7 +212,7 @@ function plugin_order_dropdownReceptionActions($type,$referenceID,$orderID) {
    }
 		
 	$ORDER_RESTRICTED_TYPES[]=	SOFTWARELICENSE_TYPE;
-	$ORDER_RESTRICTED_TYPES[]=	SOFTWARE_TYPE;
+	//$ORDER_RESTRICTED_TYPES[]=	SOFTWARE_TYPE;
 	$ORDER_RESTRICTED_TYPES[]=	CONTRACT_TYPE;
 	if (!in_array($type, $ORDER_RESTRICTED_TYPES))
 		echo "<option value='generation'>" . $LANG['plugin_order']['delivery'][3] . "</option>";
