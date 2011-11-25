9fb2cfeb9c7d1e5012386252e13f861763de99ce
tsmr <tsmr@thetsmr.fr>
2011-11-25 15:39:07 +0000

    order item bill corrections
    git-svn-id: https://forge.glpi-project.org/svn/order/trunk@583 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/order_item.class.php
+++ inc/order_item.class.php
@@ -987,8 +987,8 @@ class PluginOrderOrder_Item extends CommonDBChild {
             $item     = new $itemtype();
             echo "<tr><th><ul><li>";
             echo "<a href=\"javascript:showHideDiv('generation$rand','generation', '".
-               GLPI_ROOT."/pics/plus.png','".GLPI_ROOT."/pics/moins.png');\">";
-            echo "<img alt='' name='generation' src=\"".GLPI_ROOT."/pics/plus.png\">";
+               $CFG_GLPI["root_doc"]."/pics/plus.png','".$CFG_GLPI["root_doc"]."/pics/moins.png');\">";
+            echo "<img alt='' name='generation' src=\"".$CFG_GLPI["root_doc"]."/pics/plus.png\">";
             echo "</a>";
             echo "</li></ul></th>";
             echo "<th>" . $LANG['plugin_order']['detail'][6] . "</th>";
