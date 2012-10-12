c0c6817d35fdc136ea40d01ae828ffefcb7db1c4
tsmr <tsmr@thetsmr.fr>
2012-10-12 08:13:22 +0000

    Update for using with 0.83
    git-svn-id: https://forge.glpi-project.org/svn/order/trunk@681 349b9182-4a13-0410-896f-e5e9767dd1b3

--- ajax/referencetree.php
+++ ajax/referencetree.php
@@ -34,7 +34,7 @@ include (GLPI_ROOT."/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
-checkLoginUser();
+Session::checkLoginUser();
 
 if (!isset($_GET['target'])) {
    $_GET['target']=$CFG_GLPI['root_doc']."/plugins/order/front/reference.php";
@@ -42,6 +42,6 @@ if (!isset($_GET['target'])) {
 
 PluginOrderReference::showSelector($_GET['target']);//
 
-ajaxFooter();
+Html::ajaxFooter();
 
 ?>
--- ajax/referencetreetypes.php
+++ ajax/referencetreetypes.php
@@ -35,7 +35,7 @@ include (GLPI_ROOT."/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
-checkLoginUser();
+Session::checkLoginUser();
 
 if (isset($_REQUEST['node'])) {
   /* if ($_SESSION['glpiactiveprofile']['interface']=='helpdesk') {
