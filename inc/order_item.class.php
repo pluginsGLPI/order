1ce7e70593b554e55995f649ee820966a03785b7
Alexandre Delaunay <adelaunay@teclib.com>
2013-09-13 14:31:47 +0000

    fix delete item link
    git-svn-id: https://forge.glpi-project.org/svn/order/trunk@786 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/order_item.class.php
+++ inc/order_item.class.php
@@ -45,6 +45,10 @@ class PluginOrderOrder_Item extends CommonDBRelation {
    static public $items_id_2 = 'items_id';
    static public $checkItem_2_Rights = self::DONT_CHECK_ITEM_RIGHTS;
    
+   static public $check_entity_coherency = false;
+
+   //TODO better right and entity menber (ex Computer_Item)
+   
    static function canCreate() {
       return plugin_order_haveRight('order', 'w');
    }
