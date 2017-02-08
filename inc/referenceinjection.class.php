372b8f9a957ad0fbefbb194f2f4ad71373c343a9
François Legastelois <flegastelois@teclib.com>
2017-02-08 09:21:55 +0100

    remove unicode whitespaces

--- inc/referenceinjection.class.php
+++ inc/referenceinjection.class.php
@@ -35,18 +35,18 @@ class PluginOrderReferenceInjection extends PluginOrderReference implements Plug
    public function __construct() {
       $this->table = getTableForItemType(get_parent_class($this));
    }
-   
+
    /**
     * Returns the name of the table used to store this object parent
     *
     * @return string (table name)
    **/
-   static function getTable() {
-    
-      $parenttype = get_parent_class();
-      return $parenttype::getTable();
-   } 
- 
+   static function getTable() {
+
+      $parenttype = get_parent_class();
+      return $parenttype::getTable();
+   }
+
    public function isPrimaryType() {
       return true;
    }
