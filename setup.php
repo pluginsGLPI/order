9d61a4954af9ad439bd2eb65afd5af0d7106997f
Cédric Anne <cedric.anne@gmail.com>
2023-05-25 15:34:17 +0200

    2.10.4

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_ORDER_VERSION', '2.10.3');
+define('PLUGIN_ORDER_VERSION', '2.10.4');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ORDER_MIN_GLPI", "10.0.0");
