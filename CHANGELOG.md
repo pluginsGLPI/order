9266dede042ed1eb0efb408d789ac5e2c1afa060
stonebuzz <skita@teclib.com>
2025-07-10 11:12:17 +0200

    2.11.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [2.11.0] - 2025-07-10
+
 ### Fixed
 
 - Improved access control checks when updating user preferences
--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_ORDER_VERSION', '2.10.7');
+define('PLUGIN_ORDER_VERSION', '2.11.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ORDER_MIN_GLPI", "10.0.0");
