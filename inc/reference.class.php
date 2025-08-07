5c40ea299ce57a005f3c281b0e2e80f1426dccc2
RomainLvr <lecouvreurromain@gmail.com>
2025-08-07 15:08:14 +0200

    Fix phpstan

--- inc/reference.class.php
+++ inc/reference.class.php
@@ -381,11 +381,11 @@ class PluginOrderReference extends CommonDBTM
         $ong = [];
         if (!$this->isNewItem()) {
             $this->addDefaultFormTab($ong);
-            $this->addStandardTab('PluginOrderReference_Supplier', $ong, $options);
+            $this->addStandardTab(PluginOrderReference_Supplier::class, $ong, $options);
             // $this->addStandardTab('PluginOrderReference', $ong,$options);
-            $this->addStandardTab('Document_Item', $ong, $options);
-            $this->addStandardTab('Note', $ong, $options);
-            $this->addStandardTab('Log', $ong, $options);
+            $this->addStandardTab(Document_Item::class, $ong, $options);
+            $this->addStandardTab(Notepad::class, $ong, $options);
+            $this->addStandardTab(Log::class, $ong, $options);
         }
         return $ong;
     }
