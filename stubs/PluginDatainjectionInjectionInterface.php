0525113dc45660391a3df21aa901b581d259756d
Rom1-B <rom1.biot@gmail.com>
2025-08-13 12:00:40 +0200

    phpcbf

--- stubs/PluginDatainjectionInjectionInterface.php
+++ stubs/PluginDatainjectionInjectionInterface.php
@@ -28,4 +28,6 @@
  * -------------------------------------------------------------------------
  */
 
-interface PluginDatainjectionInjectionInterface {}
+interface PluginDatainjectionInjectionInterface
+{
+}
